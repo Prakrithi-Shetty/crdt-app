93ff9234a3253bf90e6252b6a7abb5d697b41020
Prakrithi-Shetty <prakrithishetty8072@gmail.com>
2023-08-16 15:27:47 +0530

    crdt

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState,useRef } from 'react'
+import Editor from "@monaco-editor/react";
+import * as Y from "yjs";
+import { WebrtcProvider } from 'y-webrtc';
+import { MonacoBinding} from 'y-monaco';
+
 
 function App() {
+const editorRef=useRef(null);
+
+  const handleEditorDidMount=(editor,monaco)=>{
+    editorRef.current=editor 
+    const doc= new Y.Doc();
+    const provider=new WebrtcProvider("test-room",doc);
+    const type=doc.getText("monaco");
+    const binding=new MonacoBinding(type,editorRef.current.getModel(),new Set([editorRef.current]),provider.awareness);
+    console.log(provider.awareness)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+     <Editor
+     height="100vh"
+     width="100vw"
+     thme="vs-dark"
+     onMount={handleEditorDidMount}
+     />
+    </>
+  )
 }
 
-export default App;
+export default App
